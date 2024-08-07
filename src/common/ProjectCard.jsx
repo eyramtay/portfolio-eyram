4e3029bee2499c931237bf95e4b6d73b06c72d43
eyramtay <71682389+eyramtay@users.noreply.github.com>
2024-08-07 16:37:26 -0500

    made website responsive

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -6,7 +6,10 @@ function ProjectCard({ src, link, h3, p }) {
       <img className="hover" src={src} alt={`${h3} logo`} />
       <h3>{h3}</h3>
       <p>{p}</p>
+      <br />
       <a href={link} target="_blank">
+        {" "}
+        <br />
         <button className="hover">Demo</button>
       </a>
     </section>
